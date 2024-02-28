85e8e75928c231cabe9d7427f500a8069f5a45f6
Rade Stojanovic <stojanovicrade614@gmail.com>
2024-02-28 18:17:34 +0100

    Finished setup for OpenGLCore library

--- OpenGLCore/src/Platform/Windows/WindowsWindow.cpp
+++ OpenGLCore/src/Platform/Windows/WindowsWindow.cpp
@@ -1,7 +1,9 @@
 #include "glpch.hpp"
 #include "WindowsWindow.hpp"
 
-#include"GLCore/Events/ApplicationEvent.hpp"
+#include "GLCore/Events/ApplicationEvent.hpp"
+#include "GLCore/Events/MouseEvent.hpp"
+#include "GLCore/Events/KeyEvent.hpp"
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
@@ -46,9 +48,9 @@ void WindowsWindow::Init(const WindowProps &props)
     GLCORE_ASSERT(status, "Failed to initualize Glad!");
 
     LOG_INFO("OpenGL info:");
-    LOG_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
-    LOG_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
-    LOG_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));
+    LOG_INFO("  Vendor: {0}", (char *)glGetString(GL_VENDOR));
+    LOG_INFO("  Renderer: {0}", (char *)glGetString(GL_RENDERER));
+    LOG_INFO("  Version: {0}", (char *)glGetString(GL_VERSION));
 
     glfwSetWindowUserPointer(m_Window, &m_Data);
     SetVSync(true);
@@ -57,13 +59,81 @@ void WindowsWindow::Init(const WindowProps &props)
         WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
         data.Width = width;
         data.Height = height;
-        // WindowResizeEvent event(width, height);
-        // data.EventCallback(event);
+
+        WindowResizeEvent event(width, height);
+        data.EventCallback(event);
     });
 
     glfwSetWindowCloseCallback(m_Window, [](GLFWwindow *window) {
         WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
         WindowCloseEvent event;
+        data.EventCallback(event);
+    });
+
+    glfwSetKeyCallback(m_Window, [](GLFWwindow *window, int key, int scancode, int action, int mods) {
+        WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
+
+        switch (action)
+        {
+        case GLFW_PRESS:
+        {
+            KeyPressedEvent event(key, 0);
+            data.EventCallback(event);
+            break;
+        }
+        case GLFW_RELEASE:
+        {
+            KeyReleasedEvent event(key);
+            data.EventCallback(event);
+            break;
+        }
+        case GLFW_REPEAT:
+        {
+            KeyPressedEvent event(key, 1);
+            data.EventCallback(event);
+            break;
+        }
+        }
+    });
+
+    glfwSetCharCallback(m_Window, [](GLFWwindow *window, uint32_t keycode) {
+        WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
+
+        KeyTypedEvent event(keycode);
+        data.EventCallback(event);
+    });
+
+    glfwSetMouseButtonCallback(m_Window, [](GLFWwindow *window, int button, int action, int mods) {
+        WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
+
+        switch (action)
+        {
+        case GLFW_PRESS:
+        {
+            MouseButtonPressedEvent event(button);
+            data.EventCallback(event);
+            break;
+        }
+        case GLFW_RELEASE:
+        {
+            MouseButtonReleasedEvent event(button);
+            data.EventCallback(event);
+            break;
+        }
+        }
+    });
+
+    glfwSetScrollCallback(m_Window, [](GLFWwindow *window, double xOffset, double yOffset) {
+        WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
+
+        MouseScrolledEvent event((float)xOffset, (float)yOffset);
+        data.EventCallback(event);
+    });
+
+    glfwSetCursorPosCallback(m_Window, [](GLFWwindow *window, double xPos, double yPos) {
+        WindowData &data = *(WindowData *)glfwGetWindowUserPointer(window);
+
+        MouseMovedEvent event((float)xPos, (float)yPos);
         data.EventCallback(event);
     });
 }
