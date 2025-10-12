a5ed7ace42a6571b0ee716a0206a24d7e3e63cb0
Rade Stojanovic <stojanovicrade614@gmail.com>
2025-10-12 08:51:53 +0200

    add space

--- VoxelEngine/assets/shaders/voxel.frag.glsl
+++ VoxelEngine/assets/shaders/voxel.frag.glsl
@@ -27,6 +27,7 @@ float NormalizeRadiance(int radiance, float ambient, float maxRadiance) {
 float CalculateLighting() {
     float normalizedRadiance = NormalizeRadiance(max(0, i_Fragment.Radiance), 0, 0.8);
     float radianceFromHeight = float(i_Fragment.FragmentHeight - 48) / float(u_RadianceGridHeight);
+
     return clamp(normalizedRadiance + radianceFromHeight, 0.25, 1.0);
 }
 
