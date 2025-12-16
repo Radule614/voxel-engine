56137175daf72a58f25536bf2b1127f05210eb07
Rade Stojanovic <stojanovicrade614@gmail.com>
2025-12-16 19:56:01 +0100

    pbr shader

--- VoxelEngine/assets/shaders/pbr.frag.glsl
+++ VoxelEngine/assets/shaders/pbr.frag.glsl
@@ -2,63 +2,111 @@
 
 layout (location = 0) out vec4 o_Color;
 
-in o_Vertex {
+in o_Vertex
+{
     vec3 FragNormal;
-    vec3 FragPos;
+    vec3 FragPosition;
     vec2 FragTexCoords;
 } i_Fragment;
 
-struct DirectionalLight {
-    vec3 Direction;
-    vec3 Ambient;
-    vec3 Diffuse;
-    vec3 Specular;
+struct PointLight
+{
+    vec3 LightPosition;
+    vec3 LightColor;
 };
 
-struct Material {
-    vec4 Diffuse;
-};
+uniform PointLight[] u_PointLights;
 
-uniform DirectionalLight u_DirectionalLight;
-uniform vec3 u_CameraPos;
+uniform vec3 u_CameraPosition;
 
+// Material stuff
 uniform bool u_HasBaseTexture;
 uniform vec4 u_BaseColorFactor;
-uniform sampler2D u_BaseTexture;
+uniform sampler2D u_Albedo;
+uniform float u_Metallic;
+uniform float u_Roughness;
+uniform float u_AmbientOcclusion;
 
-vec3 CalculateDirectionalLight(DirectionalLight light, Material material, vec3 normal, vec3 viewDir) {
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(-light.Direction);
-    float diff = max(dot(norm, lightDir), 0.0);
+const float PI = 3.14159265359;
 
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-    float spec = pow(max(dot(norm, halfwayDir), 0.0), 128);
-
-    vec3 ambient = light.Ambient * 0.3 * vec3(material.Diffuse);
-    vec3 diffuse = light.Diffuse * diff * vec3(material.Diffuse);
-
-    return (ambient + diffuse);
+vec3 FreshnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 
-Material CreateMaterial() {
-    Material material;
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
 
-    if (u_HasBaseTexture) {
-        material.Diffuse = texture(u_BaseTexture, i_Fragment.FragTexCoords);
-    } else {
-        material.Diffuse = u_BaseColorFactor;
-    }
+    float num = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
 
-    return material;
+    return num / denom;
 }
 
-void main() {
-    Material material = CreateMaterial();
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r * r) / 8.0;
 
-    if (material.Diffuse.a < 0.5) {
-        discard;
+    float num = NdotV;
+    float denum = NdotV * (1.0 - k) + k;
+
+    return num / denum;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+void main()
+{
+    vec3 N = normalize(i_Fragment.FragNormal);
+    vec3 V = normalize(u_CameraPosition - i_Fragment.FragPosition);
+
+    vec3 Lo = vec3(0.0);
+    for (int i = 0; i < 4; ++i)
+    {
+        vec3 L = normalize(u_PointLights[i].LightPosition - i_Fragment.FragPosition);
+        vec3 H = normalize(V + L);
+
+        float distance = length(u_PointLights[i].LightPosition - i_Fragment.FragPosition);
+        float attenuation = 1.0 / (distance * distance);
+        vec3 radiance = u_PointLights[i].LightColor * attenuation;
+
+        vec3 F0 = vec3(0.04);
+        F0 = mix(F0, u_Albedo, u_Metallic);
+        vec3 F = FreshnelSchlick(max(dot(H, V), 0.0), F0);
+
+        float NDF = DistributionGGX(N, H, u_Roughness);
+        float G = GeometrySmith(N, V, L, u_Roughness);
+
+        vec3 numerator = NDF * G * F;
+        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) * 0.0001;
+        vec3 specular = numerator / denominator;
+
+        vec3 kS = F;
+        vec3 kD = vec3(1.0) - kS;
+
+        kD *= 1.0 - u_Metallic;
+
+        float NdotL = max(dot(N, L), 0.0);
+        Lo += (kD * u_Albedo / PI + specular) * radiance * NdotL;
+
+        vec3 ambient = vec3(0.03) * u_Albedo * u_AmbientOcclusion;
+        vec3 color = ambient + Lo;
+
+        color = color / (color + vec3(1.0));
+        color = pow(color, vec3(1.0 / 2.2));
     }
-
-    vec3 viewDir = normalize(u_CameraPos - i_Fragment.FragPos);
-    o_Color = vec4(CalculateDirectionalLight(u_DirectionalLight, material, i_Fragment.FragNormal, viewDir), 1.0);
 }
--- VoxelEngine/assets/shaders/pbr.vert.glsl
+++ VoxelEngine/assets/shaders/pbr.vert.glsl
@@ -1,12 +1,12 @@
 #version 450 core
 
-layout(location = 0) in vec3 i_Position;
-layout(location = 1) in vec3 i_Normal;
-layout(location = 2) in vec2 i_TexCoords;
+layout (location = 0) in vec3 i_Position;
+layout (location = 1) in vec3 i_Normal;
+layout (location = 2) in vec2 i_TexCoords;
 
 out o_Vertex {
     vec3 FragNormal;
-    vec3 FragPos;
+    vec3 FragPosition;
     vec2 FragTexCoords;
 } o_VertexOut;
 
@@ -14,12 +14,12 @@ uniform mat4 u_ViewProjection;
 uniform mat4 u_Model;
 
 void main() {
-    vec4 pos = u_Model * vec4(i_Position, 1.0f);
+    vec4 position = u_Model * vec4(i_Position, 1.0f);
     vec3 normal = mat3(transpose(inverse(u_Model))) * i_Normal;
 
     o_VertexOut.FragNormal = normal;
     o_VertexOut.FragTexCoords = i_TexCoords;
-    o_VertexOut.FragPos = vec3(pos);
+    o_VertexOut.FragPosition = vec3(position);
 
-    gl_Position = u_ViewProjection * pos;
+    gl_Position = u_ViewProjection * position;
 }
