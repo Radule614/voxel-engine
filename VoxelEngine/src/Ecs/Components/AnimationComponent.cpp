e21839f0926f84e457589bc313d46848427bbccd
Rade Stojanovic <stojanovicrade614@gmail.com>
2026-02-17 22:05:29 +0100

    empty animation component

---
+++ VoxelEngine/src/Ecs/Components/AnimationComponent.cpp
@@ -0,0 +1,10 @@
+//
+// Created by RadU on 2/17/2026.
+//
+
+#include "AnimationComponent.hpp"
+
+namespace VoxelEngine
+{
+
+}
---
+++ VoxelEngine/src/Ecs/Components/AnimationComponent.hpp
@@ -0,0 +1,19 @@
+//
+// Created by RadU on 2/17/2026.
+//
+
+#pragma once
+
+namespace VoxelEngine
+{
+
+struct AnimationComponent
+{
+// TODO: Define animation structure and load it from model
+
+private:
+
+
+};
+
+}
